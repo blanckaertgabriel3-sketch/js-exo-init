766c8e819774db6f0983579a98e5052a4a8d35a4
gabriel <blanckaertgabriel@Gabriel>
2025-12-20 10:09:26 +0100

    modifications textuelles + Modif style le tout en JavaScript

--- manipulation-elements/script.js
+++ manipulation-elements/script.js
@@ -1,11 +1,24 @@
 window.addEventListener("load", function(){
+
+	//modifications textuelles 
 	const message = document.getElementById("emplacement-libre");
 	message.textContent = "Bonjour la page est chargée";
-});
 
-
-
-window.addEventListener("load", function(){
 	const Miam = document.querySelector(".texte-actu-cuisine");
 	Miam.textContent = "Miam Miam............................";
+
+	//modifications style
+	const hautPage = document.getElementById("haut-page");
+	hautPage.style.backgroundColor = "rgba(125, 215, 146, 1)";
+
+	const tousElements = document.querySelectorAll("*");
+	tousElements.forEach(function(tousElements) {
+		tousElements.style.margin = "0";
+		tousElements.style.padding = "0";
+	});
+
+	const uppercase = document.querySelectorAll(".uppercase");
+	uppercase.forEach(function(uppercase) {
+		uppercase.style.textTransform = "uppercase";
+	});
 });
