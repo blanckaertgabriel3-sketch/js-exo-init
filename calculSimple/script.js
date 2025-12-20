fa4517f4e297a9d54eec89f1a801f23ebbf7354e
gabriel <blanckaertgabriel@Gabriel>
2025-12-20 10:54:45 +0100

    demande utilisateur 2 nombre, retour du résustat

---
+++ calculSimple/script.js
@@ -0,0 +1,4 @@
+const nombre1 = Number(prompt("Entrez le nombre 1 : "));
+const nombre2 = Number(prompt("Entrez le nombre 2 : "));
+const resultat = nombre1 + nombre2;
+alert(nombre1 + " + " + nombre2 + " = " + resultat);
