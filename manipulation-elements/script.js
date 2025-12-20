81cb8dc8b1eecc661f98f2a9f6b7d0794ef2f1c4
gabriel <blanckaertgabriel@Gabriel>
2025-12-20 09:14:32 +0100

    changement d'elements dans page html avec js

---
+++ manipulation-elements/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+	const message = document.getElementById("emplacement-libre");
+	message.textContent = "Bonjour la page est chargée";
+});
+
+
+
+window.addEventListener("load", function(){
+	const Miam = document.querySelector(".texte-actu-cuisine");
+	Miam.textContent = "Miam Miam............................";
+});
